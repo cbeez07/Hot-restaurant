b5d5232f2a67ea2af85be8cf7042758f10b0921c
Chris B <beesleychc@gmail.com>
2018-04-18 19:44:33 -0500

    commiting routes

--- server.js
+++ server.js
@@ -9,9 +9,49 @@ let PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+let seated = [
+    {
+        customerName: "Meadow Lietzen",
+        customerPhoneNumber: "7779990000",
+        customerEmail: "Meadow@lala.com",
+        customerID: 1
+    },
+    {
+        customerName: "Sam Fisher",
+        customerPhoneNumber: "8887770000",
+        customerEmail: "samfisher@lala.com",
+        customerID: 42
+    },
+    {
+        customerName: "Elizabeth Banks",
+        customerPhoneNumber: "7773338989",
+        customerEmail: "banks@lala.com",
+        customerID: 23
+    }
+]
+
+let waiting = [];
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "home.html"));
 });
+
+app.get("/api/seated", (req, res) => {
+    return res.json(seated);
+})
+
+app.get("/api/waiting", (req, res) => {
+    return res.json(waiting);
+})
+
+app.get("/tables", (req, res) => {
+    res.sendFile(path.join(__dirname, "tables.html"));
+})
+
+app.get("/reserve", (req, res) => {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+})
+
 
 
 
